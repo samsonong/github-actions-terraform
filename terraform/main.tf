18fba61a669ba96f9ffa65b76c22ac63e9d40784
Samson Ong <contact@samsonong.com>
2023-02-10 22:21:35 +0800

    Try basic workflow + main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+    required_version = ">= 0.12.0"
+    required_providers {
+        google = {
+            source = "hashicorp/google",
+            version = "4.39.0"
+        }
+    }
+    backend "gcs" {
+        bucket = "to_be_overidden_by_terraform_init"
+    }
+}
+
+provider "google" {
+    project = "github-terraform-gcp"
+    region = "asia-southeast1"
+}
+
+data "terraform_remote_state" "db_inst" {
+    backend = "gcs"
+    config = {
+        bucket  = var.db_instance_tfstate_bucket
+    }
+    workspace = var.infra_namespace == "main" || var.infra_namespace == "pre-main" ? var.infra_namespace : "common-nonprod"
+}
+
+resource "google_cloud_run_service" "app_service" {
+    depends_on = [null_resource.push_image]
+    name = "a-prime-${local.abridged_namespace}"
+    location = "asia-southeast1"
+    template {
+        spec {
+            containers {
+                image = "gcr.io/google-samples/hello-app:1.0"
+            }
+        }
+    }
+    autogenerate_revision_name = true
+}
+
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_service.run_service.name
+  location = google_cloud_run_service.run_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+output "service_url" {
+  value = google_cloud_run_service.run_service.status[0].url
+}
