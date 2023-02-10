43b38d8286f780e2e20395e11c1595019d0f97bf
Samson Ong <contact@samsonong.com>
2023-02-10 22:40:36 +0800

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,4 @@
-variable "google_project_id" {
-    type = string
-}
-
-variable "infra_namespace" {
-    type = string
-}
-
-variable "commit_hash" {
-    type = string
+variable "project_id" {
+  description = "The project ID to deploy to"
+  type        = string
 }
