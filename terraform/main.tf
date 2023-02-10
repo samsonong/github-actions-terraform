b59b0fb145d899766fadf3972ca6b9c5bba6fb9d
Samson Ong <contact@samsonong.com>
2023-02-10 22:44:58 +0800

    a

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,8 +27,3 @@ module "cloud_run" {
   image                 = "us-docker.pkg.dev/cloudrun/container/hello"
   service_account_email = module.service_account.email
 }
-
-
-output "service_url" {
-  value = google_cloud_run_service.run_service.status[0].url
-}
