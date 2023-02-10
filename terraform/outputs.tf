667f5bd54b1db7c5d3ca6fa6de5b33be3fa3e53b
Samson Ong <contact@samsonong.com>
2023-02-10 22:41:01 +0800

    update

---
+++ terraform/outputs.tf
@@ -0,0 +1,35 @@
+output "service_name" {
+  value       = module.cloud_run.service_name
+  description = "Name of the created service"
+}
+
+output "revision" {
+  value       = module.cloud_run.revision
+  description = "Deployed revision for the service"
+}
+
+output "service_url" {
+  value       = module.cloud_run.service_url
+  description = "The URL on which the deployed service is available"
+}
+
+output "service_id" {
+  value       = module.cloud_run.service_id
+  description = "Unique Identifier for the created service"
+}
+
+output "service_status" {
+  value       = module.cloud_run.service_status
+  description = "Status of the created service"
+}
+
+output "service_location" {
+  value       = module.cloud_run.location
+  description = "Location in which the Cloud Run service was created"
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
